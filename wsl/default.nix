ed4ca2fa5463c6899ad5b6cce39fe1079cc866f0
Evgenii Kontsevoi <instane@gmail.com>
2024-06-23 17:46:17 +0000

    Enable wsl docker integration

--- wsl/default.nix
+++ wsl/default.nix
@@ -1,19 +1,34 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
+let
+  dockerRoot = ''"C:\Program Files\Docker\Docker\resources"'';
+in
 {
   system.stateVersion = "24.05";
-  wsl.enable = true;
-  wsl.defaultUser = "nixos";
-  wsl.useWindowsDriver = true;
 
-  programs.zsh.enable = true;
-  users.defaultUserShell = pkgs.zsh;
-  environment.shells = with pkgs; [ zsh ];
+  wsl = {
+    enable = true;
+    defaultUser = "nixos";
+    useWindowsDriver = true;
+    docker-desktop.enable = true;
+  };
+
+  systemd.services.docker-desktop-proxy = {
+    path = [ pkgs.mount ];
+    script = "${config.wsl.wslConf.automount.root}/wsl/docker-desktop/docker-desktop-user-distro proxy --docker-desktop-root ${config.wsl.wslConf.automount.root}/wsl/docker-desktop ${dockerRoot}";
+  };
 
   # Enable the Flakes feature and the accompanying new nix command-line tool
   nix.settings.experimental-features = [
     "nix-command"
     "flakes"
   ];
-  environment.systemPackages = with pkgs; [ git ];
+
+  environment = {
+    systemPackages = with pkgs; [ git ];
+    shells = with pkgs; [ zsh ];
+  };
+  programs.zsh.enable = true;
+  users.defaultUserShell = pkgs.zsh;
 }
+
